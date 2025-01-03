661fa99411fe617ae42e727a5aa12852341e8363
Jhonatan Becerra <yonkitas9@gmail.com>
2025-01-03 02:10:53 -0500

    reestructuración

---
+++ front/src/components/assets/Icons.jsx
@@ -0,0 +1,271 @@
+export const TechIcon = () => (
+    <svg
+        class="text-primary-600"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    >
+        <path d="M13 3l0 7l6 0l-8 11l0 -7l-6 0l8 -11" />
+    </svg>
+);
+
+export const SecurityIcon = () => (
+    <svg
+        class="text-primary-600"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        width="24"
+        height="24"
+        stroke-width="2"
+    >
+        <path d="M12 3a12 12 0 0 0 8.5 3a12 12 0 0 1 -8.5 15a12 12 0 0 1 -8.5 -15a12 12 0 0 0 8.5 -3"></path>
+    </svg>
+);
+
+export const DeliveryIcon = () => (
+    <svg
+        class="text-primary-600"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        width="24"
+        height="24"
+        stroke-width="2"
+    >
+        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+        <path d="M12 12h3.5"></path>
+        <path d="M12 7v5"></path>
+    </svg>
+);
+
+export const SupportIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg"
+        class="text-primary-600"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        width="24" height="24"
+        stroke-width="2">
+        <path d="M8 21l8 0"></path>
+        <path d="M12 17l0 4"></path>
+        <path d="M7 4l10 0"></path>
+        <path d="M17 4v8a5 5 0 0 1 -10 0v-8"></path>
+        <path d="M5 9m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+        <path d="M19 9m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+    </svg>
+);
+
+
+export const CorporateIcon = () => (
+    <svg
+        class="text-blue-600"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    >
+        <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+        <path d="M3.6 9h16.8" />
+        <path d="M3.6 15h16.8" />
+        <path d="M11.5 3a17 17 0 0 0 0 18" />
+        <path d="M12.5 3a17 17 0 0 1 0 18" />
+    </svg>
+);
+
+export const ResponsiveIcon = () => (
+    <svg
+        class="text-blue-600"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    >
+        <path d="M6 5a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2v-14z" />
+        <path d="M11 4h2" />
+        <path d="M12 17v.01" />
+    </svg>
+);
+
+export const DesignIcon = () => (
+    <svg
+        class="text-blue-600"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    >
+        <path d="M12 21a9 9 0 0 1 0 -18c4.97 0 9 3.582 9 8c0 1.06 -.474 2.078 -1.318 2.828c-.844 .75 -1.989 1.172 -3.182 1.172h-2.5a2 2 0 0 0 -1 3.75a1.3 1.3 0 0 1 -1 2.25" />
+        <path d="M8.5 10.5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+        <path d="M12.5 7.5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+        <path d="M16.5 10.5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+    </svg>
+);
+
+export const SeoIcon = () => (
+    <svg
+        class="text-blue-600"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    >
+        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+        <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+        <path d="M13.41 10.59l2.59 -2.59" />
+        <path d="M7 12a5 5 0 0 1 5 -5" />
+    </svg>
+);
+
+export const CleanCodeIcon = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="text-primary-600 stroke-2"
+        width={32}
+        height={32}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+    >
+        <path d="M7 8l-4 4l4 4"></path>
+        <path d="M17 8l4 4l-4 4"></path>
+        <path d="M14 4l-4 16"></path>
+    </svg>
+);
+
+export const LogoIcon = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="text-primary-600 stroke-2"
+        width={32}
+        height={32}
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+    >
+        <path d="M7 8l-4 4l4 4"></path>
+        <path d="M17 8l4 4l-4 4"></path>
+        <path d="M14 4l-4 16"></path>
+    </svg>
+);
+
+export const LocationIcon = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="hover:text-primary-400"
+        width="24"
+        height="24"
+        fill="none"
+        stroke="currentColor"
+    >
+        <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+        <path
+            d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z"
+        ></path>
+    </svg>
+);
+
+export const PhoneIcon = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="hover:text-primary-400"
+        width="24"
+        height="24"
+        fill="none"
+        stroke="currentColor"
+    >
+        <path
+            d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"
+        ></path>
+    </svg>
+);
+
+export const MailIcon = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="hover:text-primary-400"
+        width="24"
+        height="24"
+        fill="none"
+        stroke="currentColor"
+    >
+        <path
+            d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"
+        ></path>
+        <path d="M3 7l9 6l9 -6"></path>
+    </svg>
+);
+
+export const LinkedInIcon = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        stroke="currentColor"
+    >
+        <path
+            d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"
+        ></path>
+        <path d="M8 11l0 5"></path>
+        <path d="M8 8l0 .01"></path>
+        <path d="M12 16l0 -5"></path>
+        <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
+    </svg>
+);
+
+export const MainLogoIcon = () => (
+    <div class="flex items-center space-x-3 h-8" >
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="text-primary-600 stroke-2"
+            width={32}
+            height={32}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round" >
+            <path d="M7 8l-4 4l4 4" />
+            <path d="M17 8l4 4l-4 4" />
+            <path d="M14 4l-4 16" />
+        </svg>
+        <span className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary-600 to-purple-600">
+            WebCraft
+        </span>
+    </div>
+);
