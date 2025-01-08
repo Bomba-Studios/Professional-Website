d9726241590cb363606cc0fa839ad2abdbc95826
DanielBohorquez0425 <danielbohorquez0425@gmail.com>
2025-01-07 20:07:23 -0500

    burger menu - mobile

--- front/src/components/assets/Icons.jsx
+++ front/src/components/assets/Icons.jsx
@@ -1,3 +1,37 @@
+export const CloseButtonIcon = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        >
+        <path d="M18 6l-12 12" />
+        <path d="M6 6l12 12" />
+    </svg>
+)
+
+export const BurguerMenuIcon = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        >
+        <path d="M4 6l16 0" />
+        <path d="M4 12l16 0" />
+        <path d="M4 18l16 0" />
+    </svg>
+)
 export const TechIcon = () => (
     <svg
         class="text-primary-600"
