c222af364e2a28a0f46ee4355bfab83f28cff312
Jhonatan Becerra <yonkitas9@gmail.com>
2025-01-08 02:11:59 -0500

    metadata robots update

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import solidJs from '@astrojs/solid-js';
 
@@ -7,7 +8,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind()],
+  site: 'https://bombaweb.pages.dev/',
+  integrations: [solidJs(), tailwind(), sitemap()],
   vite: {
     assetsInclude: ['**/*.webp']
   }
